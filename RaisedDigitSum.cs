894b0b62c318388afac28ac605b1d1d125c999d0
OFFICEKIEV\s.ivaniuk <s.ivaniuk@temabit.com>
2018-05-29 10:43:59 +0300

    first commit of this exercise

--- RaisedDigitSum.cs
+++ RaisedDigitSum.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections.Generic;
 
+public class SumDigPower
+{
+
+    public static long[] SumDigPow(long a, long b)
+    {
+        List<long> list = new List<long>();
+
+        for (int i = (int)a; i <= b; i++)
+        {
+            if (Summ(i) == i)
+                list.Add(i);
+        }
+
+        long[] result = new long[list.Count];
+
+        for (int i = 0; i < list.Count; i++)
+            result[i] = list[i];
+
+        return result;
+    }
+
+    public static double Summ(int num)
+    {
+        double result = 0;
+        int digit = 0;
+        char[] ar = num.ToString().ToCharArray();
+
+        for (int i = 0; i < ar.Length; i++)
+        {
+            digit = Int32.Parse(ar[i].ToString());
+            result += Math.Pow(digit, i + 1);
+        }
+
+        return result;
+    }
+}
